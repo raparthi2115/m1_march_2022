2503e85bb86323e7b35b4241410f07c5f8d37258
raparthi2115 <101785191+raparthi2115@users.noreply.github.com>
2022-04-03 16:19:10 +0530

    Update cricketbook.h

--- 3_Implementation/inc/cricketbook.h
+++ 3_Implementation/inc/cricketbook.h
@@ -1,1 +1,16 @@
+/**
+ * @file cricketbook.h
+ * @brief 
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 
+ * 
+ */
+#ifdef __BOOKCRICKET_H__
+#define __BOOKCRICKET_H__
 
+int GetPlayerToss(char player_1[],char player_2[],int oversToplay);
+
+int batting(char battingplayer[],char bowlingplayer[],int oversToplay);
+
+#endif
