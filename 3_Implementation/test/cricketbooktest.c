ad691d58ffbc5f9d12a2420bc947577add7b20c8
raparthi2115 <101785191+raparthi2115@users.noreply.github.com>
2022-04-03 16:25:58 +0530

    Update cricketbooktest.c

--- 3_Implementation/test/cricketbooktest.c
+++ 3_Implementation/test/cricketbooktest.c
@@ -1,1 +1,46 @@
+/**
+ * @file cricketbooktest.c
+ * @brief 
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 
+ * 
+ */
 
+#include "unity.h"
+
+/* Modify these two lines according to the project */
+#include "cricketbook.h"
+#define PROJECT_NAME   "Cricket_Book"
+
+/* Prototypes for all the test functions */
+void test_GetPlayerToss(void);
+
+
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_GetPlayerToss);
+  
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_GetPlayerToss(void) {
+  TEST_ASSERT_EQUAL(7,GetPlayerToss("David Bhai","Kane Mama",1));
+ /* TEST_ASSERT_EQUAL(7,GetPlayerToss("Bairstow","Bhuvaneswar",1));*/
+  
+}
